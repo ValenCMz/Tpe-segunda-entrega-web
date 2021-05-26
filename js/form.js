738d0e62bee0b6548e20b7037ca558a62b3c99cc
camila <camilarsilva@hotmail.com.ar>
2021-05-26 16:58:18 -0300

    tabla dinamica

--- js/form.js
+++ js/form.js
@@ -1,9 +1,44 @@
-document.getElementById("valor1").innerHTML = sortearValor1();
-document.getElementById("valor2").innerHTML = sortearValor2();
-document.getElementById("botonEnviar").addEventListener("click", validarFormulario);
-let boton = document.getElementById("botonEnviar");
-let warning = document.getElementById("reintentar");
-let inputDeshabilitado = document.getElementById("inputCaptcha");
+"use strict"
+
+document.addEventListener("DOMContentLoaded", iniciarPagina);
+
+function iniciarPagina() {
+    document.getElementById("valor1").innerHTML = sortearValor1();
+    document.getElementById("valor2").innerHTML = sortearValor2();
+    let boton = document.getElementById("botonEnviar");
+    let warning = document.getElementById("reintentar");
+    let pedidos = [{
+        "cliente": "Pablo Lamponne",
+        "articulo": "Geek",
+        "talle": "M",
+        "color": "Blanco"
+    }, {
+        "cliente": "Juan Carlos Batman",
+        "articulo": "Urbana",
+        "talle": "M",
+        "color": "Negro"
+    }, {
+        "cliente": "María Laura Santillán",
+        "articulo": "Crossfit",
+        "talle": "S",
+        "color": "Gris"
+    }];
+
+    document.getElementById("botonEnviar").addEventListener("click", function (e) {
+        validarFormulario(warning, boton, pedidos)
+    });
+    insertarPrecargada(pedidos);
+
+    let btnMultiplicar = document.querySelector("#agregarVarios");
+    btnMultiplicar.addEventListener("change", function (e) {
+        agregarVarios(pedidos, this.value);
+    });
+
+    let btnBorrarPedidos = document.getElementById("borrar");
+    btnBorrarPedidos.addEventListener("click", function (e) {
+        borrarPedidos(pedidos);
+    });
+}
 
 function sortearValor1() {
     let valor1 = (Math.random() * 10).toFixed(0);
@@ -17,15 +52,16 @@ function sortearValor2() {
     return valor2;
 }
 
-function validarFormulario() {
+function validarFormulario(warning, boton, pedidos) {
     let inputUsuario = document.getElementById("inputCaptcha");
     let valorUsuario = inputUsuario.value;
     let suma = Number(valor1.innerHTML) + Number(valor2.innerHTML);
     if (valorUsuario == suma) {
         warning.innerHTML = "";
-        enviarFormulario();
+        enviarFormulario(boton);
         enviado.classList.add = ("confirmacion");
-        enviado.innerHTML = "¡Gracias por su compra!";
+        enviado.innerHTML = "Pedido Registrado";
+        agregarPedido(pedidos);
     } else {
         warning.innerHTML = "Incorrecto. Por favor, reintentar.";
         sortearValor1();
@@ -33,8 +69,56 @@ function validarFormulario() {
     }
 }
 
-function enviarFormulario() {
+function enviarFormulario(boton) {
     boton.value = "Enviado";
-    boton.disabled = true;
-    inputDeshabilitado.disabled = true;
+}
+
+function insertarPrecargada(pedidos) {
+    let tabla = document.getElementById("tabla-dinamica");
+    tabla.innerHTML = "";
+    for (let elem of pedidos) {
+        let pedido = document.createElement('tr');
+        let cliente = document.createElement('td');
+        let articulo = document.createElement('td');
+        let talle = document.createElement('td');
+        let color = document.createElement('td');
+        cliente.innerHTML = elem.cliente;
+        articulo.innerHTML = elem.articulo;
+        talle.innerHTML = elem.talle;
+        color.innerHTML = elem.color;
+        pedido.appendChild(cliente);
+        pedido.appendChild(articulo);
+        pedido.appendChild(talle);
+        pedido.appendChild(color);
+        tabla.appendChild(pedido);
+    }
+}
+
+function agregarPedido(pedidos) {
+    let cliente = document.getElementById("cliente").value;
+    let articulo = document.getElementById("articulo").value;
+    let talle = document.getElementById("talle").value;
+    let color = document.getElementById("color").value;
+    let nuevoPedido = {
+        cliente: cliente,
+        articulo: articulo,
+        talle: talle,
+        color: color
+    }
+    pedidos.push(nuevoPedido);
+    insertarPrecargada(pedidos);
+}
+
+function agregarVarios(pedidos, multiplicador) {
+    console.log(multiplicador);
+    let ultimoPedido = pedidos[pedidos.length - 1];
+    for (let i = 0; i < Number(multiplicador); i++) {
+        pedidos.push(ultimoPedido);
+    }
+    insertarPrecargada(pedidos);
+}
+
+function borrarPedidos(pedidos) {
+    pedidos.splice(0, pedidos.length)
+    insertarPrecargada(pedidos);
 }
